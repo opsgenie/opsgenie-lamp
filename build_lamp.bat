1dc0bc57e71382c1f34a960c81eb3ecdfb01add7
Cem Küçük <ckucuk@atlassian.com>
2020-03-16 16:24:23 +0300

    Revert "Change packaging dep to go-mod"
    This reverts commit 11b5af51

--- build_lamp.bat
+++ build_lamp.bat
@@ -6,16 +6,11 @@ setlocal
 set GOOS=windows
 set GOARCH=386
 echo building for %GOOS%/%GOARCH%
-go build -mod=vendor
+go build
 
 set GOOS=linux
 set GOARCH=386
 echo building for %GOOS%/%GOARCH%
-go build -mod=vendor
-
-set GOOS=darwin
-set GOARCH=386
-echo building for %GOOS%/%GOARCH%
-go build -mod=vendor -o opsgenie-lamp-mac
+go build
 
 endlocal
--- lamp.go
+++ lamp.go
@@ -11,7 +11,7 @@ import (
 	"os"
 )
 
-const lampVersion string = "3.2.0"
+const lampVersion string = "3.1.2"
 
 var commonFlags = []gcli.Flag{
 	gcli.BoolFlag{
