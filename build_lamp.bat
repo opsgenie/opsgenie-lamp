ee289c74f9d29f18d69df27164a7e78f35ece3c3
Cem Küçük <ckucuk@atlassian.com>
2020-03-16 17:01:47 +0300

    Remove unnecessary -mod flag

--- build_lamp.bat
+++ build_lamp.bat
@@ -6,16 +6,16 @@ setlocal
 set GOOS=windows
 set GOARCH=386
 echo building for %GOOS%/%GOARCH%
-go build -mod=vendor
+go build
 
 set GOOS=linux
 set GOARCH=386
 echo building for %GOOS%/%GOARCH%
-go build -mod=vendor
+go build
 
 set GOOS=darwin
 set GOARCH=386
 echo building for %GOOS%/%GOARCH%
-go build -mod=vendor -o opsgenie-lamp-mac
+go build -o opsgenie-lamp-mac
 
 endlocal
